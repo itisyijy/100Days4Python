ca2deb9973a8af84d2afa9269417cede63743076
itisyijy <jy02060577@gmail.com>
2024-09-01 22:04:50 +0900

    make string concatenation part, and organize order of contents

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,7 +1,12 @@
 #Band Name Generator
 import sys
+from tokenize import String
 
-print("--print function--")
+print("--string concatenation--")
+print("Lee" + "Jeongyun")
+print("Lee" + " " + "Jeongyun")
+
+print("\n--print function--")
 print("Hello", "World!", sep=" ", end="\n", file=sys.stdout, flush=True)
 #function의 def를 확인하자.
 #   *values: object,                            출력할 객체를 ','로 구분해 전달.
@@ -10,6 +15,7 @@ print("Hello", "World!", sep=" ", end="\n", file=sys.stdout, flush=True)
 #   file: SupportsWrite[str] | None = None,     출력을 어디에 보낼지 결정. default = sys.stdout(표준출력)
 #   flush: Literal[False] = False) -> None      출력 buffer를 강제로 비울지 결정. default = false. true 설정 시 buffer 비우고 강제 출력.
 
-print("\n--string concatenation--")
-print("Lee" + "Jeongyun")
-print("Lee" + " " + "Jeongyun")
+print("\n--input function--")
+print("Hello, ", input("What is your name >>> "), "!", sep="")
+#   prompt로 user에게 입력할 data에 대한 hint 제공. cursor를 통해 user부터 data를 입력받음.
+#   nested function -> input 먼저 실행 후 print 실행.
