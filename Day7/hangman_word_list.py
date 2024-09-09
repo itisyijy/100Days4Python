16f127a3bdb087e0f8577b58dc66a5ab2de44e1e
itisyijy <jy02060577@gmail.com>
2024-09-09 21:13:41 +0900

    DAY 7 : make hangman module and hangman program

---
+++ Day7/hangman_word_list.py
@@ -0,0 +1,31 @@
+# Day7 for 100Days4Python
+# Project for Day7
+
+word_list = [
+    "apple", "apricot", "avocado", "astronaut", "antelope", "artist", "aircraft", "alligator", "architect", "alphabet",
+    "banana", "bicycle", "butterfly", "balloon", "broccoli", "buffalo", "bathtub", "bonfire", "bookshelf", "birthday",
+    "catapult", "calendar", "camera", "chocolate", "champion", "carpenter", "crocodile", "calculator", "concert", "compass",
+    "dolphin", "dinosaur", "diamond", "drummer", "dessert", "dragonfly", "detective", "dancer", "doorway", "dashboard",
+    "elephant", "envelope", "engine", "eagle", "earthquake", "espresso", "emerald", "evolution", "elevator", "eclipse",
+    "flamingo", "festival", "fountain", "football", "forest", "fireworks", "fingerprint", "firefighter", "fortune", "freedom",
+    "gorilla", "galaxy", "guitar", "glacier", "giraffe", "gymnast", "gardener", "genius", "gratitude", "gemstone",
+    "helicopter", "hurricane", "harmonica", "habitat", "harvest", "hammock", "horizon", "honeycomb", "highway", "historian",
+    "iguana", "iceberg", "industry", "internet", "island", "ignition", "illusion", "insect", "invitation", "inventory",
+    "jellyfish", "jungle", "jigsaw", "journalist", "juggler", "jewelry", "jackpot", "jasmine", "journey", "justice",
+    "kangaroo", "kingdom", "keyboard", "knowledge", "karate", "kaleidoscope", "knight", "kitten", "kitchen", "koala",
+    "lavender", "labyrinth", "lightning", "lemonade", "landscape", "lighthouse", "literature", "locomotive", "loyalty", "labyrinth",
+    "moonlight", "museum", "mountain", "melody", "magician", "microphone", "marshmallow", "mansion", "mermaid", "mosaic",
+    "narwhal", "necklace", "nebula", "notebook", "navigator", "needle", "nightingale", "ninja", "nectar", "novelty",
+    "octopus", "orchestra", "oasis", "ostrich", "orbit", "oatmeal", "olive", "orange", "otter", "oxygen",
+    "pineapple", "parachute", "pyramid", "penguin", "passport", "piano", "puzzle", "pepper", "popcorn", "pumpkin",
+    "quokka", "quasar", "quiver", "quartz", "queen", "quiz", "quilt", "question", "quota", "quiet",
+    "rhinoceros", "rainbow", "robot", "raspberry", "raccoon", "rocket", "river", "revolver", "recipe", "rabbit",
+    "submarine", "sunflower", "spaceship", "sculpture", "stadium", "sapphire", "skateboard", "satellite", "suitcase", "squirrel",
+    "tornado", "treasure", "triangle", "telescope", "trampoline", "tulip", "thunderstorm", "toaster", "turtle", "tattoo",
+    "unicorn", "umbrella", "universe", "ukulele", "underground", "uranium", "utensil", "ultimate", "upward", "undertake",
+    "volcano", "violin", "vampire", "victory", "vanilla", "vulture", "velvet", "voyage", "vision", "vehicle",
+    "wizard", "waterfall", "wanderer", "walrus", "wildfire", "whistle", "werewolf", "waffle", "wallet", "wilderness",
+    "xylophone", "xenon", "xerox", "xylophone", "xenial", "xeroderma", "xenia", "xenophobia", "xiphoid",
+    "yogurt", "yacht", "yawn", "yellow", "yardstick", "yodel", "yoke", "yeti", "yearbook", "youth",
+    "zebra", "zucchini", "zeppelin", "zombie", "zipper", "zenith", "zest", "zigzag", "zookeeper", "zircon"
+]
