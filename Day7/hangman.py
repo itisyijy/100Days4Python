f6ca6f3fb9f5639574fd0c362845a5337e9850d2
itisyijy <jy02060577@gmail.com>
2024-09-09 21:14:39 +0900

    ???

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,2 +1,39 @@
 # Day7 for 100Days4Python
 # Project for Day7
+
+import random
+import hangman_word_list
+import hangman_ascii_art
+
+print(hangman_ascii_art.logo)
+
+lives = 6
+word_list = hangman_word_list.word_list
+stages = hangman_ascii_art.stages
+chosen_word = list(random.choice(word_list))
+print("".join(chosen_word))
+
+placeholder = []
+for i in chosen_word:
+    placeholder.append("_")
+
+display = placeholder
+while display != chosen_word and lives != 0:
+    print(" ".join(display))
+    guess = input("Guess a letter >>> ").lower()
+    if guess in display:
+        print(f"**********You've already guessed {guess}.**********")
+        continue
+    for index in range(len(chosen_word)):
+        if guess == chosen_word[index]:
+            display[index] = guess
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"******* {guess} is not in the word. Lives -1*******")
+    print(stages[lives])
+    print(f"***************{lives}/6 Lives Left.***************\n\n")
+
+if lives == 0:
+    print("******************YOU LOSE*******************")
+else:
+    print("*******************YOU WIN*******************")
