8078db6b5f2a45cc9f90378a48aefad1af83ec01
itisyijy <jy02060577@gmail.com>
2024-09-10 20:33:42 +0900

    DAY8 : file name modified >> param_args.py

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,3 +1,21 @@
 # Day8 for 100Days4Python
 # Project for Day8
 # Caesar Cipher
+
+alphabet_list = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+#input_direction = input("[encode] for encrypt, [decode] for decrypt >>> ")
+#input_text = input("Type your message >>> ")
+#input_shift = int(input("Type the shift amount >>> "))
+
+def encrypt(original, shift):
+    result = ""
+    for letter in original:
+        if letter in alphabet_list:
+            result += alphabet_list[(alphabet_list.index(letter) + shift) % 26]
+        else:
+            result += letter
+    return result
