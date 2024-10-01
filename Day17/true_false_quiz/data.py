e6957458c9a2130cefb77687a4337286e3f08e06
itisyijy <jy02060577@gmail.com>
2024-10-01 23:19:44 +0900

    test

--- Day17/true_false_quiz/data.py
+++ Day17/true_false_quiz/data.py
@@ -1,3 +1,6 @@
+# Day17 for 100Days4Python
+# Day17 : True False Quiz
+
 question_data = [
 {"text": "A slug's blood is green.", "answer": "True"},
 {"text": "The loudest animal is the African Elephant.", "answer": "False"},
