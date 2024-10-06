3b8ce69daf855a83262d4a187345eeb21deb56e1
itisyijy <jy02060577@gmail.com>
2024-10-06 19:50:56 +0900

    Day22 : Create the screen

---
+++ Day22/main.py
@@ -0,0 +1,21 @@
+# Day22 for 100Days4Python
+# Project for Day22 : The Pong Game
+
+from turtle import Screen
+
+# Create the screen
+screen = Screen()
+
+screen.title("Pong")
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+
+# Create and move a paddle
+# Create another paddle
+# Create the ball and make it move
+# Detect collision with wall and bounce
+# Detect collision with paddle
+# Detect when paddle misses
+# Keep score
+
+screen.exitonclick()
