0648d641675c7ff57fcbdc88c998544cb2155380
itisyijy <jy02060577@gmail.com>
2024-10-24 19:56:55 +0900

    Day26 : loop with data frame pandas

--- Day26/dict_comprehension.py
+++ Day26/dict_comprehension.py
@@ -1,2 +1,32 @@
 # Day26 for 100Days4Python
 # Day26 : Dictionary Comprehension -> only available in python
+
+import random
+import pandas
+
+# Dictionary Comprehension : Create new dictionary from previous dictionary
+# Pattern : new_dict = {new_key:new_value for item in list}
+names = ['Li', 'Mia', 'Zoe', 'Finn', 'Yara', 'Jonathan', 'Alexander', 'Elizabeth', 'Gabrielle', 'Christopher']
+students_scores = {student: random.randint(1, 100) for student in names}
+print(students_scores)
+
+# Pattern : new_dict = {new_key:new_value for (key,value) in dict.items()}
+# Pattern : new_dict = {new_key:new_value for (key,value) in dict.items() if condition}
+passed_students = {student: score for (student, score) in students_scores.items() if score > 60}
+print(passed_students)
+players = {
+    "name": ["Son", "Maddison", "Solanke", "Kulusevski", "Romero"],
+    "number": [7, 10, 19, 21, 17],
+}
+
+# Loop through a data frame
+players_df = pandas.DataFrame(players)
+print(players_df)
+
+for (key, value) in players_df.items():
+    print(key)
+    print(value)
+
+# Loop through rows of a data frame
+for (index, row) in players_df.iterrows():
+    print(row["name"], row["number"])
